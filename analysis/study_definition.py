633205e70e1ad94f14dd515608df217ca390ec25
LFISHER7 <lhf282@live.co.uk>
2021-03-04 15:13:42 +0000

    prev covid vars

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -29,38 +29,40 @@ study = StudyDefinition(
             on_or_before="index_date",
             returning="binary_flag",
         ),
+
+        # https://github.com/opensafely/risk-factors-research/issues/49
+        age=patients.age_as_of(
+            "index_date",
+            return_expectations={
+                "rate": "universal",
+                "int": {"distribution": "population_ages"},
+                "incidence" : 0.001
+            },
+        ),
+        
+        # https://github.com/opensafely/risk-factors-research/issues/46
+        sex=patients.sex(
+            return_expectations={
+                "rate": "universal",
+                "category": {"ratios": {"M": 0.49, "F": 0.51}},
+            }
+        ),
+
     ),
 
 
-    # https://github.com/opensafely/risk-factors-research/issues/49
-    age=patients.age_as_of(
-        "index_date",
-        return_expectations={
-            "rate": "universal",
-            "int": {"distribution": "population_ages"},
-            "incidence" : 0.001
-        },
-    ),
-    
-    # https://github.com/opensafely/risk-factors-research/issues/46
-    sex=patients.sex(
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"M": 0.49, "F": 0.51}},
-        }
-    ),
-
+    #did they attend ae ever
     attended_ae = patients.attended_emergency_care(
         between=["index_date", end_date],
         returning="binary_flag",
         find_first_match_in_period=True,
-        date_format="YYYY-MM-DD",
         return_expectations={
             "incidence": 0.05
         }
         
     )
 
+    # date of ae attendance
     attended_ae_date = patients.attended_emergency_care(
         between=["index_date", end_date],
         returning="date_arrived",
@@ -68,32 +70,45 @@ study = StudyDefinition(
         date_format="YYYY-MM-DD",
     )
 
-    attended_ae_for_covid = patients.attended_emergency_care(
+    # covid status of those admitted to ae
+    attended_ae_covid_status = patients.attended_emergency_care(
         between=["index_date", end_date],
         returning="binary_flag",
         find_first_match_in_period=True,
         with_these_diagnoses=covid_codes,
     )
 
-    attended_ae_for_respiratory = patients.attended_emergency_care(
-        between=["index_date", end_date],
-        returning="binary_flag",
-        find_first_match_in_period=True,
-        with_these_diagnoses=respiratory_codes,
-    )
-
-    recent_positive_covid_test_before_admission = patients.with_test_result_in_sgss(
+    # in those attending ae had they had recent positiv cov test
+    positive_covid_test_before_admission = patients.with_test_result_in_sgss(
         pathogen="SARS-CoV-2",
         test_result="positive",
         between=["attended_ae_date - 2 weeks", "attended_ae_date"],
         returning="binary_flag",
-        date_format="YYYY-MM-DD",
         return_expectations={
             "date": {"earliest": "2021-01-01",  "latest" : "2021-02-01"},
             "rate": "exponential_increase",
             "incidence": 0.1,
         },
     ),
+
+    # in those attending ae had they had positive cov in pc
+    covid_primary_care_before_admission = patients.with_these_clinical_events(
+        codelist=covid_primary_care_codes,
+        between=["attended_ae_date - 2 weeks", "attended_ae_date"],
+        returning="binary_flag",
+        return_expectations={
+            "date": {"earliest": "2021-01-01",  "latest" : "2021-02-01"},
+            "rate": "exponential_increase",
+            "incidence": 0.1,
+        },
+    ),
+    
+    # does ae attendance lead to hosp admission
+    ae_preceding_hospital_admission = patients.admitted_to_hospital(
+        between["attended_ae_date", "attended_ae_date + 2 days"],
+        returning="binary_flag",
+        return_expectations={"incidence": 0.9}
+    )
 
 
 )
