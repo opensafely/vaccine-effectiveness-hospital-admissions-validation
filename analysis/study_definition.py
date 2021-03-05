6b01652c6c66f5011d2ae2b8ccee18132da44f58
LFISHER7 <lhf282@live.co.uk>
2021-03-05 17:50:24 +0000

    ae attendance followed by hosp discharge

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -7,7 +7,7 @@ start_date="2020-09-01"
 end_date="2021-01-01"
 
 ae_discharge_dict = {"discharged_to_ward": 306706006, "discharged_to_icu": 1066391000000105}
-# ae_discharge_list = [key for (key, value) in ae_discharge_dict.items()]
+ae_discharge_list = [value for (key, value) in ae_discharge_dict.items()]
 
 
 
@@ -32,7 +32,7 @@ study = StudyDefinition(
         AND
         NOT has_died
         AND
-        (covid_hospital_admission OR other_hospital_admission OR ae_attendance)
+        (covid_hospital_admission OR ae_attendance)
         """,
         registered=patients.registered_as_of(
             "index_date",
@@ -62,23 +62,27 @@ study = StudyDefinition(
     ),
 
 
-    #did they attend ae ever
+
+    #did they attend ae ever and discharged to hosp or icu
     ae_attendance = patients.attended_emergency_care(
         between=["index_date", end_date],
         returning="binary_flag",
         find_last_match_in_period=True,
+        discharged_to=ae_discharge_list,
         return_expectations={
             "incidence": 0.4
         }
         
     ),
 
-    # date of ae attendance
+    # want people who attended ae then go on to be admitted to hosp
     ae_attendance_date = patients.attended_emergency_care(
-        between=["index_date", end_date],
         returning="date_arrived",
         find_last_match_in_period=True,
-        date_format="YYYY-MM-DD",
+        discharged_to=ae_discharge_list,
+        return_expectations = {
+            "incidence": 0.5,
+            "category": {"ratios": {306706006: 0.5, 1066391000000105: 0.5}},}
     ),
 
     # covid status of those attendance to ae
@@ -92,8 +96,10 @@ study = StudyDefinition(
         }
     ),
 
+    
+
     # date of ae attendance due to covid
-    ae_attendance_covid_status = patients.attended_emergency_care(
+    ae_attendance_covid_status_date = patients.attended_emergency_care(
         between=["index_date", end_date],
         returning="date_arrived",
         find_last_match_in_period=True,
@@ -116,8 +122,21 @@ study = StudyDefinition(
         },
     ),
 
+    # looks at more historical positive cov test
+    positive_covid_test_before_ae_attendance_month = patients.with_test_result_in_sgss(
+        pathogen="SARS-CoV-2",
+        test_result="positive",
+        between=["ae_attendance_date - 28 days", "ae_attendance_date -14 days"],
+        returning="binary_flag",
+        return_expectations={
+            "date": {"earliest": "2021-01-01",  "latest" : "2021-02-01"},
+            "rate": "exponential_increase",
+            
+        },
+    ),
+
     # in those attending ae had they had positive cov in pc
-    covid_primary_care_before_admission = patients.with_these_clinical_events(
+    covid_primary_care_before_ae_attendance = patients.with_these_clinical_events(
         codelist=covid_primary_care_codes,
         between=["ae_attendance_date - 14 days", "ae_attendance_date"],
         returning="binary_flag",
@@ -128,18 +147,18 @@ study = StudyDefinition(
         },
     ),
     
-    
 
-    discharge_destination = patients.attended_emergency_care(
-        on_or_after="ae_attendance_date",
-        returning="discharge_destination",
-        find_last_match_in_period=True,
+
+    primary_covid_hospital_admission=patients.admitted_to_hospital(
+        returning="date_admitted",
+        with_these_primary_diagnoses=covid_codes,
+        on_or_after="index_date",
         date_format="YYYY-MM-DD",
-        return_expectations = {
-            "incidence": 0.5,
-            "category": {"ratios": {306706006: 0.5, 1066391000000105: 0.5}},}
+        find_first_match_in_period=True,
+        return_expectations={
+            "incidence": 0.3,
+        },
     ),
-
 
     covid_hospital_admission=patients.admitted_to_hospital(
         returning="date_admitted",
@@ -151,6 +170,7 @@ study = StudyDefinition(
             "incidence": 0.3,
         },
     ),
+
 
 
 )
